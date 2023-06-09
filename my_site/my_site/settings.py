46438186fa0d786078e53c504187602c50fb5fa3
Hello09Andrey <104667768+Hello09Andrey@users.noreply.github.com>
2023-06-09 07:46:14 +0300

    add form

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -1,4 +1,8 @@
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,3 +114,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'andrey1020907@gmail.com'#os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = 'fkzfgztdaeirwrkj'#os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'andrey1020907@gmail.com'#os.getenv('DEFAULT_FROM_EMAIL')
