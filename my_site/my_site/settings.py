b982bad7bd09ed248d7d56bc52beb492f40ba1b3
Hello09Andrey <104667768+Hello09Andrey@users.noreply.github.com>
2023-06-12 20:51:30 +0300

    comments

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,8 +117,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'andrey1020907@gmail.com'#os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = 'fkzfgztdaeirwrkj'#os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'andrey1020907@gmail.com'#os.getenv('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
