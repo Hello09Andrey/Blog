8c610b1e1ab4402fde5d82b4478164850fc05bc7
Hello09Andrey <104667768+Hello09Andrey@users.noreply.github.com>
2023-06-24 07:39:00 +0300

    sitemap

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -19,6 +19,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -31,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
